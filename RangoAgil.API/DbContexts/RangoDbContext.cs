70357d8e038a50e91655d91b9b9fd4b4e44f20a0
CaioLavor <caiolavor@outlook.com.br>
2025-04-23 23:41:05 -0300

    .

--- RangoAgil.API/DbContexts/RangoDbContext.cs
+++ RangoAgil.API/DbContexts/RangoDbContext.cs
@@ -5,9 +5,7 @@ namespace RangoAgil.API.DbContexts;
 
 public class RangoDbContext(DbContextOptions<RangoDbContext> options) : DbContext(options)
 {
-    public DbSet<Rango> Rangos { get; set; } = null;
-    public DbSet<Ingrediente> Ingredientes { get; set; } = null;
-
+    public DbSet<Rango> Rangos { get; set; } = null;    public DbSet<Ingrediente> Ingredientes { get; set; } = null;
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         _ = modelBuilder.Entity<Ingrediente>().HasData(
--- RangoAgil.API/Program.cs
+++ RangoAgil.API/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RangoAgil.API.DbContexts;
+using RangoAgil.API.Entities;
+using AutoMapper;
+using RangoAgil.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,8 +12,49 @@ builder.Services.AddDbContext<RangoDbContext>(
     o => o.UseSqlite(builder.Configuration["ConnectionStrings:RangoDbConnectionString"])
     );
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/rangos", async Task<Results<NoContent, Ok<List<Rango>>>>
+    (RangoDbContext rangoDbContext,
+    [FromQuery(Name = "name")] string? rangoNome) =>
+{
+    var rangosEntity = await rangoDbContext.Rangos
+    .Where(x => rangoNome == null || x.Nome.Contains(rangoNome))
+    .ToListAsync();
+
+    if (rangosEntity.Count <= 0 || rangosEntity == null)
+
+        return TypedResults.NoContent();
+
+    else
+
+        return TypedResults.Ok(rangosEntity);
+
+});
+
+app.MapGet("/rango/{rangoId:int}/ingredientes", async (
+    RangoDbContext rangoDbContext,
+    IMapper mapper,
+    int rangoId) =>
+{
+    return mapper.Map<IEnumerable<IngredienteDTO>> ((await rangoDbContext.Rangos
+    .Include(rango => rango.Ingredientes)
+    .FirstOrDefaultAsync(rango => rango.Id == rangoId))?.Ingredientes);
+}
+);
+
+app.MapGet("/rango/{id:int}", async (RangoDbContext rangoDbContext, int id) =>
+{
+    return await rangoDbContext.Rangos.FirstOrDefaultAsync(x => x.Id == id);
+});
+
+app.MapGet("/rango", async (RangoDbContext rangoDbContext) =>
+{
+    return await rangoDbContext.Rangos.ToListAsync();
+});
 
 app.Run();
